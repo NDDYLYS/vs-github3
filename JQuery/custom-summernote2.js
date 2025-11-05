5e21c80d59f33626b44c65252dc57f866b8a0a48
NDDYLYS <nodvic@naver.com>
2025-11-05 09:27:05 +0900

    asd

---
+++ JQuery/custom-summernote2.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(".summernote-editor").summernote({
+        height: 250, // set editor height
+        minHeight: 200, // set minimum height of editor
+        maxHeight: 400, // set maximum height of editor
+
+        placeholder:"타인 죽는다?",
+        toolbar:[
+        ["font", ["style", "fontname", "fontsize", "forecolor", "backcolor"]],
+        ["style", ["bold", "italic", "underline", "strikethrough"]],
+        ["attach", ["picture", "video"]],
+        ["tool", ["ol", "ul", "paragraph", "table", "hr", "fullscreen"]],
+        ],
+    });
+});
