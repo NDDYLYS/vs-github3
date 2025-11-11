031474559d3c0a7e528e2d9030d3589efa88416e
NDDYLYS <nodvic@naver.com>
2025-11-11 10:32:10 +0900

    phone.react

--- reactjs/demo05/src/App.jsx
+++ reactjs/demo05/src/App.jsx
@@ -1,69 +1,85 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
+import './show-div.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function App() {
   const [text, setText] = useState("");
 
   return (
     <>
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
-          <div className="col">
-            <label>{text}</label>
-          </div>
-        </div>
+          <div className="col-md-8 offset-md-2 col-sm-10 offset-sm-1 ">
+            <div className="row">
+                <div className="col">
+                    <div className="bg-dark text-light p-4 rounded-4">
+                        <h1>휴대폰 React</h1>
+                        <p>Phone.React</p>
+                    </div>
+                </div>
+            </div>
 
-        <div className="row mt-1 justify-content-center">
-          <div className="col-2 p-0 me-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"7")}}>7</button>
-          </div>
-          <div className="col-2 p-0">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"8")}}>8</button>
-          </div>
-          <div className="col-2 p-0 ms-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"9")}}>9</button>
-          </div>
-        </div>
+            <div className="row">
+              <div className="col">
+                <label>{text}</label>
+              </div>
+            </div>
 
-        <div className="row mt-1 justify-content-center">
-          <div className="col-2 p-0 me-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"4")}}>4</button>
-          </div>
-          <div className="col-2 p-0">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"5")}}>5</button>
-          </div>
-          <div className="col-2 p-0 ms-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"6")}}>6</button>
-          </div>
-        </div>
+            <div className="row mt-1 justify-content-center">
+              <div className="col-2 p-0 me-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "7") }}>7</button>
+              </div>
+              <div className="col-2 p-0">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "8") }}>8</button>
+              </div>
+              <div className="col-2 p-0 ms-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "9") }}>9</button>
+              </div>
+            </div>
 
-        <div className="row mt-1 justify-content-center">
-          <div className="col-2 p-0 me-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"1")}}>1</button>
-          </div>
-          <div className="col-2 p-0">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"2")}}>2</button>
-          </div>
-          <div className="col-2 p-0 ms-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"3")}}>3</button>
-          </div>
-        </div>
+            <div className="row mt-1 justify-content-center">
+              <div className="col-2 p-0 me-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "4") }}>4</button>
+              </div>
+              <div className="col-2 p-0">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "5") }}>5</button>
+              </div>
+              <div className="col-2 p-0 ms-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "6") }}>6</button>
+              </div>
+            </div>
 
-        <div className="row mt-1 justify-content-center">
-          <div className="col-2 p-0 me-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"#")}}>#</button>
-          </div>
-          <div className="col-2 p-0">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"0")}}>0</button>
-          </div>
-          <div className="col-2 p-0 ms-1">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText(text+"$")}}>$</button>
-          </div>
-        </div>
+            <div className="row mt-1 justify-content-center">
+              <div className="col-2 p-0 me-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "1") }}>1</button>
+              </div>
+              <div className="col-2 p-0">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "2") }}>2</button>
+              </div>
+              <div className="col-2 p-0 ms-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "3") }}>3</button>
+              </div>
+            </div>
 
-        <div className="row mt-1">
-          <div className="col-6 offset-3 p-0 justify-content-center">
-            <button className="btn btn-secondary w-100" onClick={()=>{setText("")}}>전체 지우기</button>
+            <div className="row mt-1 justify-content-center">
+              <div className="col-2 p-0 me-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "#") }}>#</button>
+              </div>
+              <div className="col-2 p-0">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "0") }}>0</button>
+              </div>
+              <div className="col-2 p-0 ms-1">
+                <button className="btn btn-secondary w-100" onClick={() => { setText(text + "$") }}>$</button>
+              </div>
+            </div>
+
+            <div className="row mt-1">
+              <div className="col-6 offset-3 p-0 justify-content-center">
+                <button className="btn btn-secondary w-100" onClick={() => { setText("") }}>전체 지우기</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
