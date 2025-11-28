358384191618792c233fc47f3501de2cb55ea143
NDDYLYS <nodvic89@gmail.com>
2025-11-28 15:33:33 +0900

    결제 내역 페이지 작성

--- reactjs/home/src/components/account/AccountPayInfomation.jsx
+++ reactjs/home/src/components/account/AccountPayInfomation.jsx
@@ -1,12 +1,48 @@
+import { useCallback, useEffect } from "react";
 import Jumbotron from "../../templates/jumbotron";
 
 
 export default function AccountPayInfomation() {
 
+    const [paymentList, setPaymentList] = useState([]);
+
+    useEffect(()=>{
+        loadData();
+    });
+
+    const loadData = useCallback(async()=>{
+        const {data} = await axios.get("/payment/account");
+        setPaymentList(data);
+    }, []);
+
+    const calculateStatus = useCallback(payment=>{
+        const [paymentTotal, paymentRemain] = payment;
+        if (paymentTotal === paymentRemain) 
+            return "결제완료";
+        if (paymentRemain === 0) 
+            return "결제전체취소";
+        return "결제취소";
+    }, []);
+
     return (
 
         <>
-            <Jumbotron subject="AccountPayInfomation" detail="AccountPayInfomation"></Jumbotron>
+            {/* <Jumbotron subject="결제내역" detail="AccountPayInfomation"></Jumbotron> */}
+
+           {paymentList.map(payment=>{
+                <div className="row mt-4">
+                    <div className="col">
+                        <div>{payment.paymentName}</div>
+                        <div>금액:{payment.paymentTotal}</div>
+                        <div>번호:{payment.paymentTid}</div>
+                        <div>일시:{payment.paymentTime}</div>
+                        <div>상태:{calculateStatus}</div>
+                        <div className="mt-2">
+                            <Link className="btn btn-lg btn-primary" to="#">상세보기</Link>
+                        </div>
+                    </div>
+                </div>
+           })}
         </>
 
     )
