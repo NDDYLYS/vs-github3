1bc92e405d235688e17e127fd1dfc0c6ae3479ba
NDDYLYS <nodvic89@gmail.com>
2025-12-02 11:53:41 +0900

    채팅 내역 정상화

--- reactjs/home/src/components/websocket/WebsocketBasicEx.jsx
+++ reactjs/home/src/components/websocket/WebsocketBasicEx.jsx
@@ -2,9 +2,10 @@ import { useState, useCallback, useEffect, use } from "react";
 import Jumbotron from "../templates/Jumbotron";
 import SockJS from "sockjs-client";
 import { Client } from "@stomp/stompjs";
+import "./WebsocketBasicEx.css";
 
 // npm i sockjs-client @stomp/stompjs 설치 필요 
-export default function WebsocketBasicEx(){
+export default function WebsocketBasicEx() {
 
     const [client, setClient] = useState(null);
     const [input, setInput] = useState("");
@@ -14,7 +15,7 @@ export default function WebsocketBasicEx(){
         const client = conectToServer();
         setClient(client);
 
-        return ()=>{
+        return () => {
             disconnectFromServer(client);
             setClient(null);
         }
@@ -22,15 +23,16 @@ export default function WebsocketBasicEx(){
 
 
     // 연결 생성 > client 생성 > 구독 설정
-    const conectToServer = useCallback(()=>{
+    const conectToServer = useCallback(() => {
         //1
-        const socket = new SockJS("http://localhost:8080/ws");
+        const socket = new SockJS("http://192.168.20.30:8080/ws");
         //2
         const client = new Client({
             webSocketFactory: () => socket, // 소켓 정보 설정
             onConnect: () => { // 연결 성공 시
                 client.subscribe("/public/basic", (message) => { // 구독 설정
-                    console.log("메시지 수신 : ", message);
+                    const json = JSON.parse(message.body);
+                    setHistory((prev) => [...prev, json]);
                 });
             },
             debug: (str) => {
@@ -44,12 +46,12 @@ export default function WebsocketBasicEx(){
 
     }, []);
 
-    const disconnectFromServer = useCallback((client)=>{
+    const disconnectFromServer = useCallback((client) => {
         if (client)
             client.deactivate();
     }, []);
 
-    const sendMessage = useCallback(()=>{
+    const sendMessage = useCallback(() => {
         if (client === null)
             return;
         if (client.connected === false)
@@ -75,16 +77,30 @@ export default function WebsocketBasicEx(){
 
         <>
             <Jumbotron subject="웹소켓 basic" detail="웹소켓을 이용한 채팅 테스트 페이지입니다." />
-        
+
             <div className="row mt-4">
                 <div className="col d-flex align-items-center">
                     <input text="text" className="form-control w-auto flex-grow-1"
-                    value = {input} onChange={e=>setInput(e.target.value)} />
+                        value={input} onChange={e => setInput(e.target.value)}
+                        onKeyUp={e => {
+                            if (e.key === 'Enter') {
+                                sendMessage();
+                            }
+                        }} />
                     <button className="btn btn-primary ms-2" onClick={sendMessage}>전송</button>
-                    </div>
+                </div>
             </div>
-        
-        
+
+            <div className="row mt-4">
+                <div className="col message-wrapper">
+                    {history.map((m, index) => (
+                        <div className="message-block" key={index}>
+                            {m.content}
+                        </div> 
+                    ))}
+                </div>
+            </div>
+
         </>
 
     )
